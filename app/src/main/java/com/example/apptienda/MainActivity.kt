8c49b5f57fece1cca568dd6595b5b9519544dd12
Jhanpool <twix2907@gmail.com>
2024-12-17 20:11:30 -0500

    222 subida

--- app/src/main/java/com/example/apptienda/MainActivity.kt
+++ app/src/main/java/com/example/apptienda/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hola cambiazo",
+        text = "Hola csadadaso",
         modifier = modifier
     )
 }
