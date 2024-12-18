dd815d520f295a082730539485b5ef2b7fc35333
Jhanpool <twix2907@gmail.com>
2024-12-17 20:09:49 -0500

    segunda subida

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.apptienda"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.apptienda"
--- app/src/main/java/com/example/apptienda/MainActivity.kt
+++ app/src/main/java/com/example/apptienda/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hola mundo",
         modifier = modifier
     )
 }
